0b6b18f483a4680160f5a83494ecae3a691437d2
LiliyaAbdulova <130398lilya@gmail.com>
2018-01-23 01:32:59 +0500

    selection+time+radix

--- main.cpp
+++ main.cpp
@@ -22,15 +22,36 @@ void selection(int *a,int size)
 }
    for(i=0;i<size-1;i++)cout<<a[i]<<endl;
 }
+
+void radix(int *ar, int *br, int *cr, int sizeC, int sizeAB)
+{
+    int i,k,n;
+    k = sizeC;
+    n = sizeAB;
+    for (i = 0; i <= k; i++)
+        cr[i]=0;
+    for (i = 0; i < n; i++)
+        cr[ar[i]]+=1;
+    for (i = 1; i < k+1; i++)
+        cr[i]+=cr[i-1];
+    for (i = n-1; i >= 0; i--)
+      {
+         br[cr[ar[i]]-1]=ar[i];
+         cr[ar[i]]-=1;
+      }for(i=0;i<n-1;i++)cout<<"-"<<ar[i]<<endl;
+  }
 int main()
 {
    clock_t time;
     time = clock();
     int i,size=10000;
     int a[size];
+    int b[size];
+    int c[size];
 for(i=0;i<size;i++){
     a[i]=1+rand()%9000;
 }
+radix(a,b,c,size,size);
 selection(a,size);
 time = clock() - time;
 printf("second%f", (double)time/CLOCKS_PER_SEC);
